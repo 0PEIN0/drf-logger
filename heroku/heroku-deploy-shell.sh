0b264fecd09c1eac272b38847dbbba33385edd72
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2017-01-31 02:25:54 +0600

    Updated heroku deploy shell.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- heroku/heroku-deploy-shell.sh
+++ heroku/heroku-deploy-shell.sh
@@ -11,8 +11,8 @@ for i in ${appNames[@]}; do
   touch $i/migrations/__init__.py
 done
 echo 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;' | ./manage.py dbshell
-python manage.py heroku_deploy
-python manage.py makemigrations
-python manage.py migrate
+./manage.py heroku_deploy
+./manage.py makemigrations
+./manage.py migrate
 ./manage.py shell < heroku/drf_logger_init_data_load.py
 gunicorn drflogger.wsgi --timeout 300 --limit-request-line 8190 --limit-request-fields 200 --limit-request-field_size 8190 --keep-alive 5 --log-file -
