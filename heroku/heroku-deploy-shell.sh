c9f4f779385832f9d8d5b540c07f76bc822ab8a1
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2017-01-31 01:24:16 +0600

    Added heroku deploy shell script file.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

---
+++ heroku/heroku-deploy-shell.sh
@@ -0,0 +1,18 @@
+rm -rf */migrations/
+find . -name '*.pyc' -delete
+if [ -d "uploads" ]; then
+  echo "Upload directory exists!"
+else
+  mkdir uploads
+fi;
+appNames=(utility)
+for i in ${appNames[@]}; do
+  mkdir $i/migrations/
+  touch $i/migrations/__init__.py
+done
+echo 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;' | ./manage.py dbshell
+python manage.py heroku_deploy
+python manage.py makemigrations
+python manage.py migrate
+./manage.py shell < heroku/drf_logger_init_data_load.py
+gunicorn drflogger.wsgi --timeout 300 --limit-request-line 8190 --limit-request-fields 200 --limit-request-field_size 8190 --keep-alive 5 --log-file -
